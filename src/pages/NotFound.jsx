97de53fd3967c127afe3c1d9ff3fe4a6440773db
Yuliya Maslyanenko <yuliyamas86@gmail.com>
2023-05-31 08:47:26 +0300

    back to home

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,12 +1,33 @@
 import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import Loader from 'components/Loader/Loader';
 
 const NotFound = () => {
-  return <NotFoundContainer>Page Is Not Found</NotFoundContainer>;
+  const navigate = useNavigate();
+   useEffect(() => {
+     const timeout = setTimeout(() => {
+       navigate('/');
+     }, 3000);
+
+     return () => clearTimeout(timeout);
+   }, [navigate]);
+
+  return (
+    <NotFoundContainer>
+      {' '}
+      <h1>404 - Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+      <Loader  />
+    </NotFoundContainer>
+  );
 };
 
 
 const NotFoundContainer = styled.div`
   display: flex;
+  gap: 20px;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   height: 100vh;
